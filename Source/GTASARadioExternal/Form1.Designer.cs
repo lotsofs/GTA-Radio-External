e185c5decda26b04f51c5cd18ab0983ecf9a7171
lotsofs <oeneboy@hotmail.com>
2017-02-13 23:33:45 +0100

    Initial Build

---
+++ Source/GTASARadioExternal/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace GTASARadioExternal {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Program Status will be displayed here";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(15, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(135, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "change volume";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(234, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "20";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Program made by twitch.tv/lotsofs";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 106);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "GTASA x Winamp Radio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Source/GTASARadioExternal/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace GTASARadioExternal {
+    public partial class Form1 : Form {
+
+        Timer timer2;
+        int displayedText;
+
+        public Form1() {
+            InitializeComponent();
+            Program.InitTimer();        // run the timer that checks for updates
+            WindowTimer();              // run the timer that prints these updates
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+            // Unused but removing it throws an error :D
+        }
+
+        // print some status stuff
+        void updateWindow() {
+            if (Program.radioStatus == 2) {
+                label1.Text = "Radio ON with volume " + Program.volumeStatus;
+                label2.Text = Program.volumeStatus.ToString();
+                displayedText = 2;
+            }
+            else if (Program.radioStatus == 7 && displayedText != 7) {
+                label1.Text = "Radio OFF";
+                displayedText = 7;
+            }
+            else if (Program.radioStatus == -1 && displayedText != -1) {
+                label1.Text = "Winamp not running";
+                displayedText = -1;
+            }
+            else if (Program.radioStatus == -2 && displayedText != -2) {
+                label1.Text = "GTASA not running";
+                displayedText = -2;
+            }
+        }
+
+        // timer that checks every second for updates to be made to the printed info on the window
+        public void WindowTimer() {
+            timer2 = new Timer();
+            timer2.Tick += new EventHandler(timer2Tick);
+            timer2.Interval = 1000;
+            timer2.Start();
+        }
+
+        void timer2Tick(object sender, EventArgs e) {
+            updateWindow();
+        }
+
+        // pointless feature I added where you could enter the max volume in a box, but then made it controllable via winamp itself anyway
+        private void button1_Click(object sender, EventArgs e) {
+            /*int outThingIDontNeed;
+            if (Int32.TryParse(textBox1.Text, out outThingIDontNeed)) {
+                if (Int32.Parse(textBox1.Text) <= 255 && Int32.Parse(textBox1.Text) > 0) {
+                    Program.maxVolume = Int32.Parse(textBox1.Text);
+                    label2.Text = Int32.Parse(textBox1.Text).ToString();
+                }
+            }*/
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            // Unused but removing it throws an error :D
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+            // Unused but removing it throws an error :D
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Unused but removing it throws an error :D
+        }
+
+        private void label3_Click(object sender, EventArgs e) {
+            // Unused but removing it throws an error :D
+        }
+    }
+}
